23d4185a1143e32cec4df5025f5af2cf38ab512f
RakulAgn <119593989+RakulAgn@users.noreply.github.com>
2025-10-08 09:25:03 +0530

    feat: implement add method to handle empty string in StringCalculator

---
+++ stringCalculator.js
@@ -0,0 +1,13 @@
+class StringCalculator {
+  add(numbers) {
+    if (numbers === '') {
+      return 0;
+    }
+  }
+}
+
+/**
+ *
+ * 1. First Handle the Empty Values Properly
+ * 2. Split By Delimiter (Comma,NewLine)
+ */
